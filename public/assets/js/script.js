8afc1aba343cd8421247a897492fc108dc07c7e6
alecvipa <hebrit_626@hotmail.com>
2025-05-04 12:40:01 +0200

    readme

--- public/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-if (window.location.href == "https://www.xyncs.com/xyncsweb") {
-    $(".navBarD").hide();
-}
-
-if (window.location.href == "https://www.xyncs.com/xyncsweb") {
-    $("#fotterSmth").hide();
-}
